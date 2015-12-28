0a8926a7b0c92dd9535933fa55e7303c1f9b1116
John Haugeland <stonecypher@gmail.com>
2015-12-28 11:26:39 -0800

    cleaner

--- qn.js
+++ qn.js
@@ -17,20 +17,23 @@ function note_name(ts)     { return 'note:' + ts.toString(); }
 function set_note(ts,t)    { return set_key(note_name(ts), t); }
 function get_note(ts)      { return get_key(note_name(ts)); }
 function add_note(t)       { var ts = Date.now().toString(); set_note(ts, t); return ts; }
+function del_note(t)       { window.alert(`todo: del ${t}`); }
 function list_notes()      { return list_keys().filter(k => k.substring(0,5) === "note:").map(k => parseInt(k.substring(5), 10)) }
 
 function tag_name(ts)      { return 'tag:' + ts.toString(); }
 function get_tag(tag)      { return get_key(tag_name(tag)) || []; }
 function set_tag(tag,a)    { return set_key(tag_name(tag), a); }
 function add_tag(tag,n)    { set_tag(tag, get_tag(tag).concat([n])); }
-//function del_tag(tag,n) ...
+function del_tag(tag,n)    { window.alert(`todo: del tag ${tag} from ${n}`); }
 function list_tags()       { return list_keys().filter(k => k.substring(0,4) === "tag:").map(k => k.substring(4)); }
 
 
 
 function sl(ts)            { return make_note_link(ts); }
 function slia(t)           { return atag === t? '<div class="slia">'+(get_tag(t).map(ts => sl(ts)).join('')+'</div>') : ''; }
-function make_note_link(t) { return `<div class="item"><a class="del">⨷</a> <a class="note" onclick="anote = (anote === ${t})? '' : ${t}; byId('editor').value = '${get_note(t)}'; redraw();">${get_note(t)}</a></div>`; }
+function make_note_del(t)  { return `<a class="del" onclick="del_note(${t});">⨷</a>`; }
+function make_note_b_ln(t) { return `<a class="note" onclick="anote = (anote === ${t})? '' : ${t}; byId('editor').value = '${get_note(t)}'; redraw();">${get_note(t)}</a>`; }
+function make_note_link(t) { return `<div class="item">${make_note_del(t)} ${make_note_b_ln(t)}</div>`; }
 function make_notelist()   { return list_notes().map(t => make_note_link(t)).join(''); }
 function make_tag_link(t)  { return `<div class="item"><a class="tag" onclick="atag = (atag === '${t}')? '' : '${t}'; redraw();">${t}</a></div>${slia(t)}`; }
 function make_taglist()    { return list_tags().map(t => make_tag_link(t)).join(''); }
